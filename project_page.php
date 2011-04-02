23fef275467b90537439686e603e7afaa02793e8
apbennett <apbennett@6eb4b54e-b818-40e0-b70d-c4c03c522985>
2011-04-02 00:27:08 +0000

    Added project_page.php for viewing a project

---
+++ project_page.php
@@ -0,0 +1,16 @@
+<?php
+include("template.php");
+$page = new webPage("SNAP: Project", "", "projects");
+$page->openPage();
+$page->pageHeader();
+
+?>
+
+		<div id="main_body">
+			<div id="main_content">
+
+		</div>
+	</div>
+<?php
+$page->closePage();
+?>
