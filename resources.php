e1066a1e4c1f15035bf2d3a367176e915448b119
becrevensten <becrevensten@6eb4b54e-b818-40e0-b70d-c4c03c522985>
2011-11-02 22:51:27 +0000

    corrects tabs>spaces on some files experimentally (vs. pmd/checkstyle), removes Config.php from repository

--- resources.php
+++ resources.php
@@ -12,83 +12,83 @@ $resTypes = array ( "Paper", "Report", "Presentation", "Video");
 
 function getPublicationListSpecial() {
 
-	$resourceLayout = new ResourceLayout();
-	$resourceLayout->setRequests( $_GET );
-	$resourceLayout->fetchResources();
+    $resourceLayout = new ResourceLayout();
+    $resourceLayout->setRequests( $_GET );
+    $resourceLayout->fetchResources();
 
-	echo '<div>'.$resourceLayout->getResultsCount().$resourceLayout->getSortCriteria().$resourceLayout->getFilterReset().'</div>';
+    echo '<div>'.$resourceLayout->getResultsCount().$resourceLayout->getSortCriteria().$resourceLayout->getFilterReset().'</div>';
 
-	echo '<div id="resourceSummaries">';
-	if( $resourceLayout->resultsCount ) {
-		echo $resourceLayout->getResourceSummaryHtml();
-	}
-	echo "</div>";
+    echo '<div id="resourceSummaries">';
+    if( $resourceLayout->resultsCount ) {
+        echo $resourceLayout->getResourceSummaryHtml();
+    }
+    echo "</div>";
 
 }
 ?>
-		<div id="main_body">
-			<div id="main_content">
-				<div class="subHeader">Resources</div>
-					<form method="get" action="resources.php">
-					<div class="filters" style="position: relative; margin-top: 5px;">
-					<div style="color: #444444; font-size: 15px; margin-bottom: 6px; margin-left: 6px; font-weight: bold; display: inline-block">Publication Type</div>
-					<select name="type" style="position: absolute; left: 175px;" onchange="submit();">
-						<option value="">All</option>
-						<?php
-						for ($i = 0; $i < sizeof($resTypes); $i++){
-							$selected = "";
-							if ( isset($_GET['type']) && $_GET['type'] == $i + 1){
-								$selected = "selected=\"selected\"";
-							}
-							echo "<option $selected value=\"".($i + 1)."\">".$resTypes[$i]."</option>";
-						}
-						?>
-					</select>
-					</div>
-					<div class="filters" style="position: relative; margin-top: 5px;">
-					<div style="color: #444444; font-size: 15px; margin-bottom: 6px; margin-left: 6px; font-weight: bold; display: inline-block">Subject</div>
-					<select name="tags" style="position: absolute; left: 175px;" onchange="submit();">
-						<option value="">All</option>
-						<?php
-						$tag_query = "SELECT id, tag from resource_tags GROUP BY tag";
-						$tag_result = mysql_query($tag_query);
-						while($tag_row = mysql_fetch_array($tag_result)){
-							$selected = "";
-							if ( isset($_GET['tags']) && $_GET['tags'] == $tag_row['tag']){
-								$selected = "selected=\"selected\"";
-							}
-							echo "<option $selected value=\"".$tag_row['tag']."\">".$tag_row['tag']."</option>";
-						}
-						?>
-					</select>
-					</div>
-					<div class="filters" style="position: relative; margin-top: 5px;">
-					<div style="color: #444444; font-size: 15px; margin-bottom: 6px; margin-left: 6px; font-weight: bold; display: inline-block">Collaborator</div>
-					<select name="collab" style="position: absolute; left: 175px;" onchange="submit();">
-						<option value="">All</option>
-						<?php
-						$collab_query = "SELECT id, name from collaborators";
-						$collab_result = mysql_query($collab_query);
-						while($collab_row = mysql_fetch_array($collab_result)){
-							$selected = "";
-							if ( isset($_GET['collab']) && $_GET['collab'] == $collab_row['id']){
-								$selected = "selected=\"selected\"";
-							}
-							echo "<option $selected value=\"".$collab_row['id']."\">".$collab_row['name']."</option>";
-						}
-						?>
-					</select>
-					</div>
+        <div id="main_body">
+            <div id="main_content">
+                <div class="subHeader">Resources</div>
+                    <form method="get" action="resources.php">
+                    <div class="filters" style="position: relative; margin-top: 5px;">
+                    <div style="color: #444444; font-size: 15px; margin-bottom: 6px; margin-left: 6px; font-weight: bold; display: inline-block">Publication Type</div>
+                    <select name="type" style="position: absolute; left: 175px;" onchange="submit();">
+                        <option value="">All</option>
+                        <?php
+                        for ($i = 0; $i < sizeof($resTypes); $i++){
+                            $selected = "";
+                            if ( isset($_GET['type']) && $_GET['type'] == $i + 1){
+                                $selected = "selected=\"selected\"";
+                            }
+                            echo "<option $selected value=\"".($i + 1)."\">".$resTypes[$i]."</option>";
+                        }
+                        ?>
+                    </select>
+                    </div>
+                    <div class="filters" style="position: relative; margin-top: 5px;">
+                    <div style="color: #444444; font-size: 15px; margin-bottom: 6px; margin-left: 6px; font-weight: bold; display: inline-block">Subject</div>
+                    <select name="tags" style="position: absolute; left: 175px;" onchange="submit();">
+                        <option value="">All</option>
+                        <?php
+                        $tag_query = "SELECT id, tag from resource_tags GROUP BY tag";
+                        $tag_result = mysql_query($tag_query);
+                        while($tag_row = mysql_fetch_array($tag_result)){
+                            $selected = "";
+                            if ( isset($_GET['tags']) && $_GET['tags'] == $tag_row['tag']){
+                                $selected = "selected=\"selected\"";
+                            }
+                            echo "<option $selected value=\"".$tag_row['tag']."\">".$tag_row['tag']."</option>";
+                        }
+                        ?>
+                    </select>
+                    </div>
+                    <div class="filters" style="position: relative; margin-top: 5px;">
+                    <div style="color: #444444; font-size: 15px; margin-bottom: 6px; margin-left: 6px; font-weight: bold; display: inline-block">Collaborator</div>
+                    <select name="collab" style="position: absolute; left: 175px;" onchange="submit();">
+                        <option value="">All</option>
+                        <?php
+                        $collab_query = "SELECT id, name from collaborators";
+                        $collab_result = mysql_query($collab_query);
+                        while($collab_row = mysql_fetch_array($collab_result)){
+                            $selected = "";
+                            if ( isset($_GET['collab']) && $_GET['collab'] == $collab_row['id']){
+                                $selected = "selected=\"selected\"";
+                            }
+                            echo "<option $selected value=\"".$collab_row['id']."\">".$collab_row['name']."</option>";
+                        }
+                        ?>
+                    </select>
+                    </div>
 
 
-					</form>
+                    </form>
 
 
-					<div style="width: 900px; clear: both; height: 1px;"></div>
-					<div style="margin-top: 40px;"><?php getPublicationListSpecial(); ?></div>
+                    <div style="width: 900px; clear: both; height: 1px;"></div>
+                    <div style="margin-top: 40px;"><?php getPublicationListSpecial(); ?></div>
 
-		</div>
-	</div>
+        </div>
+    </div>
 <?php
 $page->closePage();
 ?>
--- src/Resource.php
+++ src/Resource.php
@@ -8,115 +8,115 @@ require_once('src/SwDb.php');
 */
 class Resource {
 
-	public function __construct($props) {
-		$this->props = $props;
-	}
+    public function __construct($props) {
+        $this->props = $props;
+    }
 
-	public function getTagList() {
-		$tags = '';
-		try {
-			$dbh = SwDb::getInstance();
-			$sth = $dbh->query("SELECT GROUP_CONCAT( DISTINCT tag ORDER BY tag SEPARATOR ', ') AS tags FROM resource_tags WHERE resourceid='{$this->props['id']}'");
-			$tags = $sth->fetch();
-		} catch (Exception $e) {
-			throw new Exception($e); // bubble
-		}
-		if(!empty($tags[0])) { 
-			$tags = '<div style="color: #666;">Tags: '.$tags[0].'</div>';
-		} else {
-			$tags = '';
-		}
-		return $tags;
+    public function getTagList() {
+        $tags = '';
+        try {
+            $dbh = SwDb::getInstance();
+            $sth = $dbh->query("SELECT GROUP_CONCAT( DISTINCT tag ORDER BY tag SEPARATOR ', ') AS tags FROM resource_tags WHERE resourceid='{$this->props['id']}'");
+            $tags = $sth->fetch();
+        } catch (Exception $e) {
+            throw new Exception($e); // bubble
+        }
+        if(!empty($tags[0])) { 
+            $tags = '<div style="color: #666;">Tags: '.$tags[0].'</div>';
+        } else {
+            $tags = '';
+        }
+        return $tags;
 
-	}
+    }
 
-	public function toSummaryHtml() {
+    public function toSummaryHtml() {
 
-		$tags = $this->getTagList();
+        $tags = $this->getTagList();
 
-		return <<<html
+        return <<<html
 <div id="pub_box_{$this->props['id']}" class="resource">
-	<div id="pub_hover_{$this->props['id']}" class="hover_box">
-		<div style="position: relative; ">
-			<div style="position: absolute; ">
-				<img alt="{$this->imgAltText}" src="{$this->imgSrc}" style="margin-left: 5px;" />
-			</div>
-			<div style="position: relative; left: 59px; width: 380px;">
-				<div style="font-size: 15px; color: #111111; margin-top: 5px; margin-bottom: 5px;" >
-					<a href="resource_page.php?resourceid={$this->props['id']}">{$this->props['title']}</a>
-				</div>
-				<div style="position: relative; width: 420px; margin-bottom: 10px;"><!-- no-op lint --></div>$tags
-			</div>
-		</div>
-	<div style="margin-top: 10px;width: 420px; padding: 10px;">{$this->props['summary']}</div>
-	<div style="position: relative; left: 385px; bottom: 5px; margin-top: 10px;">
-		<a id="pub_close_{$this->props['id']}" style="cursor: pointer; cursor: hand;">close &#8855;</a>
-	</div>
+    <div id="pub_hover_{$this->props['id']}" class="hover_box">
+        <div style="position: relative; ">
+            <div style="position: absolute; ">
+                <img alt="{$this->imgAltText}" src="{$this->imgSrc}" style="margin-left: 5px;" />
+            </div>
+            <div style="position: relative; left: 59px; width: 380px;">
+                <div style="font-size: 15px; color: #111111; margin-top: 5px; margin-bottom: 5px;" >
+                    <a href="resource_page.php?resourceid={$this->props['id']}">{$this->props['title']}</a>
+                </div>
+                <div style="position: relative; width: 420px; margin-bottom: 10px;"><!-- no-op lint --></div>$tags
+            </div>
+        </div>
+    <div style="margin-top: 10px;width: 420px; padding: 10px;">{$this->props['summary']}</div>
+    <div style="position: relative; left: 385px; bottom: 5px; margin-top: 10px;">
+        <a id="pub_close_{$this->props['id']}" style="cursor: pointer; cursor: hand;">close &#8855;</a>
+    </div>
 </div>
 <div style="width: 50px; padding: 6px; position: absolute; z-index: 1;">
-	<img alt="{$this->imgAltText}" src="{$this->imgSrc}" style="" />
+    <img alt="{$this->imgAltText}" src="{$this->imgSrc}" style="" />
 </div>
 <div style="position: absolute; padding-top: 6px; left: 60px; width: 380px;">
-	<div style="font-size: 15px; color: #111111; margin-bottom: 5px;" >
-		<a href="resource_page.php?resourceid={$this->props['id']}">{$this->props['title']}</a>
-	</div>
+    <div style="font-size: 15px; color: #111111; margin-bottom: 5px;" >
+        <a href="resource_page.php?resourceid={$this->props['id']}">{$this->props['title']}</a>
+    </div>
 </div>
 <script type="text/javascript">
 var config = { 
-	over: function()
-		{
-			$('#pub_hover_{$this->props['id']}').fadeIn(300);
-		},
-	interval: 100,
-	out: function()	
-		{ 
-			$('#pub_hover_{$this->props['id']}').hide(0); 
-		} 
+    over: function()
+        {
+            $('#pub_hover_{$this->props['id']}').fadeIn(300);
+        },
+    interval: 100,
+    out: function() 
+        { 
+            $('#pub_hover_{$this->props['id']}').hide(0); 
+        } 
 };
 $('#pub_box_{$this->props['id']}').hoverIntent(config);
 $('#pub_close_{$this->props['id']}').click( function()
-	{ 
-		$('#pub_hover_{$this->props['id']}').hide(0);
-	}
+    { 
+        $('#pub_hover_{$this->props['id']}').hide(0);
+    }
 );
 </script>
 </div>
 html;
 
-	}
+    }
 
-	static public function factory($props) {
+    static public function factory($props) {
 
-		switch($props['type']) {
+        switch($props['type']) {
 
-			case 1: return new ReportResource($props); break;
-			case 2: return new PaperResource($props); break;
-			case 3: return new PresentationResource($props); break;
-			case 4: return new VideoResource($props); break;
-			default: throw new Exception('Type of resource ['.$props['type'].'] was not recognized.');
+            case 1: return new ReportResource($props); break;
+            case 2: return new PaperResource($props); break;
+            case 3: return new PresentationResource($props); break;
+            case 4: return new VideoResource($props); break;
+            default: throw new Exception('Type of resource ['.$props['type'].'] was not recognized.');
 
-		}
-	}
+        }
+    }
 }
 
 class ReportResource extends Resource {
-	public $imgAltText="Report";
-	public $imgSrc = "images/pub_report.png";
+    public $imgAltText="Report";
+    public $imgSrc = "images/pub_report.png";
 }
 
 class PaperResource extends Resource {
-	public $imgAltText="Paper";
-	public $imgSrc = "images/pub_paper.png";
+    public $imgAltText="Paper";
+    public $imgSrc = "images/pub_paper.png";
 }
 
 class PresentationResource extends Resource {
-	public $imgAltText="Presentation";
-	public $imgSrc = "images/pub_presentation.png";
+    public $imgAltText="Presentation";
+    public $imgSrc = "images/pub_presentation.png";
 }
 
 class VideoResource extends Resource {
-	public $imgAltText="Video";
-	public $imgSrc = "images/pub_video.png";
+    public $imgAltText="Video";
+    public $imgSrc = "images/pub_video.png";
 }
 
 
