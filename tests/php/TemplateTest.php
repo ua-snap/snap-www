88de598c0e6870114d84df6930a2ba42672956a9
becrevensten <becrevensten@6eb4b54e-b818-40e0-b70d-c4c03c522985>
2012-01-13 22:40:13 +0000

    updates test to correctly test for jquery1.7.1

--- tests/php/TemplateTest.php
+++ tests/php/TemplateTest.php
@@ -10,7 +10,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase
     	$t = new Template();
     	$js = $t->getHeadJavascript();
     	$this->assertTag(array('tag'=>'script','attributes' => array('comment'=>'Google Analytics')), $js, "include Google Analytics");
-    	$this->assertTag(array('tag'=>'script','attributes' => array('src'=>'js/jquery-1.6.4.min.js')), $js, "include minified JQuery");
+    	$this->assertTag(array('tag'=>'script','attributes' => array('src'=>'js/jquery-1.7.1.min.js')), $js, "include minified JQuery");
     	$this->assertTag(array('tag'=>'script','attributes' => array('src'=>'js/jquery.cycle.all.js')), $js, "include jquery.cycle plugin");
     	$this->assertTag(array('tag'=>'script','attributes' => array('src'=>'http://s7.addthis.com/js/250/addthis_widget.js#username=snapweb')), $js, "include Add This widget");
     	$this->assertTag(array('tag'=>'script','attributes' => array('comment'=>'AddThis Configuration')), $js, "include code to configure the Add This widget");
