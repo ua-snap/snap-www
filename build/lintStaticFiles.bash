d47793558243d2f84a1f6dccacadc90f08f07e6c
Bruce Crevensten <becrevensten@alaska.edu>
2012-02-19 23:49:37 -0800

    adds a static asset checker to help lint images

---
+++ build/lintStaticFiles.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# This script just checks to see if there are any static image files that are 
+# referenced nowhere in the CSS/HTML/Javascript files, so they can be removed.
+
+IMAGES=/var/www/html/images/*
+
+for f in $IMAGES
+do
+	basename=`basename $f`
+	grep -qs $basename js/* *.php *.html css/*
+ 	if [ $? -ne 0 ]
+ 	then
+ 		echo "$f is not in use, DELETE $f"
+	fi
+done
