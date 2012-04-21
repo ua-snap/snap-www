7916c5cdf710dd91adf64b8eae900782629a25e2
Bruce Crevensten <becrevensten@alaska.edu>
2012-04-20 22:37:04 -0800

    adds logo to terms of service page

--- terms.php
+++ terms.php
@@ -10,6 +10,7 @@ $page->pageHeader();
             <h2>Terms of Use</h2>
 
 <p>All information produced by the Scenarios Network for Alaska and Arctic Planning is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>. Attribution text provided <a href="/faq.php#faq_1">here</a>.</p>
+<p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0; padding: 1px 1em 0 0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png"/></a></p>
 
         </div>
     </div>
