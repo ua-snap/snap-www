ac3f80c0a6b448f86e910528d79eb77916dc9bf4
Bruce Crevensten <becrevensten@alaska.edu>
2012-06-05 11:29:42 -0800

    refs #73, updates graphics on Sustainability page

--- sustainability.php
+++ sustainability.php
@@ -9,36 +9,39 @@ $page->pageHeader();
             <div id="main_content" class="methods text sustainability">
                 <h2>Sustainability</h2>
 
-<p>Whether its modeling changing ecosystems, developing scenarios, or providing information for stakeholders and decision makers, climate change is at the heart of much of SNAP&rsquo;s work. Being keenly aware of the impacts of climate change on our lives, homes, and organization, SNAP recognizes its responsibility to environmentally sustainable and ethical business practices. SNAP works with its employees, collaborators, and the University of Alaska System to develop, implement, and engage in actions that conserve natural resources. We recognize that the job is never done, and SNAP will continue to promote the wellbeing of the planet and people through environmentally conscientious behaviors. 
+<p>Whether it&rsquo;s modeling changing ecosystems, developing scenarios, or providing information for stakeholders and decision makers, climate change is at the heart of much of SNAP&rsquo;s work. Being keenly aware of the impacts of climate change on our lives, homes, and organization, SNAP recognizes its responsibility to environmentally sustainable and ethical business practices. SNAP works with its employees, collaborators, and the University of Alaska System to develop, implement, and engage in actions that conserve natural resources. We recognize that the job is never done, and SNAP will continue to promote the wellbeing of the planet and people through environmentally conscientious behaviors. 
 </p>
 
+<div>
+<img src="images/snap-green-bin-tr.png" />
 <h3>Waste Management</h3>
-<img src="images/snap-green-waste.gif" />
+<h4>The SNAP office recycles mixed paper, aluminum, tin, plastic, cardboard, glass, and electronics.</h4>
 <p>
 In collaboration with the University of Alaska Fairbanks, SNAP actively participates in recycling paper, tin, aluminum, plastic, glass and cardboard. SNAP also regularly recycles electronics, such as batteries, cell phones, and printer cartridges. Some of the food waste generated in the office is taken home and composted. SNAP tries to use as many environmentally friendly office supplies as possible, including printer paper made with recycled materials. 
 </p>
+</div>
 
-
+<div>
+<img src="images/snap-green-bik-tr.png" />
 <h3>Transportation</h3>
-<img src="images/snap-green-transport.gif" />
-
+<h4>Last year, SNAP employees commuted more than 9,000 miles by bicycle!</h4>
 <p>In their daily transportation choices, SNAP employees save almost 10,000 pounds of carbon dioxide from entering the atmosphere. On overage, SNAP employees&rsquo; commute by bike 257 miles per week in the summer. Even during the subzero temperatures of winter in Fairbanks, SNAP employees continue to commute by means other than car approximately 142 miles per week. 
 </p>
+</div>
 
-
-
+<div>
+<img src="images/snap-green-woman-tr.png" />
 <h3>Community Involvement</h3>
-<img src="images/snap-green-community.gif" />
-
-<p>
-Our staff also contributes time to volunteering for green organizations, including the <a href="http://www.arcticaudubon.org/">Arctic Audubon Society</a>, the Denali Center &ldquo;Green Thumb&rdquo; Horticultural Therapy Program, the <a href="http://northern.org/">Northern Alaska Environmental Center</a>, and <a href="http://www.calypsofarm.org/">Calypso Farm and Ecology Center</a>.
+<h4>SNAP employees contribute their talents and efforts to several local environmental organizations.</h4>
+<p>Our staff also contributes time to volunteering for green organizations, including the <a href="http://www.arcticaudubon.org/">Arctic Audubon Society</a>, the Denali Center &ldquo;Green Thumb&rdquo; Horticultural Therapy Program, the <a href="http://northern.org/">Northern Alaska Environmental Center</a>, and <a href="http://www.calypsofarm.org/">Calypso Farm and Ecology Center</a>.
 </p>
+</div>
 
-
-
+<div>
 <h3>Sustainability Initiatives</h3>
 <p>You can learn more about sustainability initiatives in the University of Alaska System by visiting the <a href="http://www.uaa.alaska.edu/sustainability/">University of Alaska Anchorage Office of Sustainability</a> or the <a href="http://www.uaf.edu/sustainability/">University of Alaska Fairbanks Office of Sustainability</a> online.
 </p>
+</div>
 
             </div>
         </div>
