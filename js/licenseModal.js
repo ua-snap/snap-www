b27ebc6da7df201de3df92014c8ff528f8cb5efb
bamelegari <bamelegari@alaska.edu>
2013-05-20 13:00:47 -0800

    further updates, backing up before dev VM wipe.

--- js/licenseModal.js
+++ js/licenseModal.js
@@ -17,15 +17,13 @@ continue the download process upon the user's acceptance of the agreement.
 
 //TODO: charts exporting window for hires png shows up before this dialog, which makes it so that it says "exporting" even after 'declined', fix?
 		//on that note -- remember that you can close that exporting dialog from anywhere with '#processingExportDialog'.dialog('close'); 
+		//possibly related -- if you click the hires download, cancel it, and then try another one, and cancel IT, the canceled window gives way to an empty download window
+				//--we'll have to see if that gets fixed when I fix the above issue.
 function licenseModal( salutation, licenseAgreement, downloadFunction ) {
 	//TODO: consider strategically placed fieldset/legend tags for formatting prettiness
-	
-	//var myDiv = $("#" + salutation)/*.append($('textEntry'))*/
-	//myDiv.append($("#" + licenseAgreement ));
-
 	var downloadDialog = $(document.createElement('div'));
-	downloadDialog.append(document.getElementById(salutation)).append(document.getElementById("textEntry"))
-		.append(document.getElementById(licenseAgreement));
+	downloadDialog.append( document.getElementById( salutation ) ).append( document.getElementById( "textEntry" ) )
+		.append( document.getElementById( licenseAgreement ) );
 
 	downloadDialog.show().dialog({
 		dialogClass: "no-close",
@@ -42,6 +40,8 @@ function licenseModal( salutation, licenseAgreement, downloadFunction ) {
 		buttons: {
 			"Accept":function() {
 				$(this).dialog('close');
+				/*var form = document.getElementById( textEntry );	//doesn't work, but wanna do something like it
+				form.submit();*/
 				downloadFunction();
 			},
 
--- src/licenseModal.php
+++ src/licenseModal.php
@@ -4,30 +4,60 @@ facilitate the creation of a new window for a specific file or type of file, it
 should be added here.-->
 
 <link rel="stylesheet" href="/css/licenseModal.css" type="text/css" />
-
-<div id="test" style="display: none;">
-	<p>TESTING</p>
-</div>
-
-
 <div style="display: none;">
+
+<!--************************************** Below here should be placed various introductory text divs -->
 	
 	<div id="foo">
 	<p>SALUTATION</p>
 	</div>
+
+
+
+
+
+<!--************************************** Below here should be placed various license agreement divs -->
 
 	<div id="bar">
 	<p>LICENSE AGREEMENT</p>
 	</div>
 
 
-	<form id="textEntry"><!--TODO: add method and action attributes and link to either mySQL or google spreadsheet-->
+
+
+
+<!--************************************** The following is added to every window -->
+
+	<form id="textEntry" method="POST">
 		<fieldset>
-			Full Name: <input type="text"><br>
-			Organization: <input type="text"><br>
-			Email Address: <input type="text"><br>
+			<p>Full Name: <input type="text" name="name"></p>
+			<p>Organization: <input type="text" name="organization"></p>
+			<p>Email Address: <input type="text" name="email"></p>
+			<p><input type="submit" value="Submit"></p>
 		</fieldset>
 	</form>
 
 </div>
 
+
+	
+<?php
+
+
+//TODO: EMAIL NOTICE CODE HERE
+
+
+$_POST['IP'] = $_SERVER['REMOTE_ADDR'];
+$_POST['download'] = "test";
+
+
+
+
+$c = curl_init("https://script.google.com/macros/s/AKfycbwwVgvDBKR-jEfBK5r9PwA7YvZpNsNy41l9fn3RhkWOz_B7BUXl/exec");
+curl_setopt($c, CURLOPT_POST, 1);
+curl_setopt($c, CURLOPT_POSTFIELDS, $_POST);
+curl_exec($c);
+curl_close($c);
+
+//https://script.google.com/macros/s/AKfycbwwVgvDBKR-jEfBK5r9PwA7YvZpNsNy41l9fn3RhkWOz_B7BUXl/exec
+?>
