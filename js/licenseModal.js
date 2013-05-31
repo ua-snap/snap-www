1355733e227f6fb4e9fd58246430d659a8e8630d
bryce <bryce@localhost.localdomain>
2013-05-31 15:27:15 -0800

    implements a cookie that will be set when user accepts so that they don't have to accept for every download. For testing purposes it currently expires after only 30 seconds -- this will change to some large amount of time with final implementation.

--- js/licenseModal.js
+++ js/licenseModal.js
@@ -15,81 +15,73 @@ continue the download process upon the user's acceptance of the agreement.
 
 */
 
-
-function sendFilename( filename ) {
-	var finalData = {
-		'download': filename
-	};
-
-	$.ajax({
-		url: 		"https://script.google.com/macros/s/AKfycbwwVgvDBKR-jEfBK5r9PwA7YvZpNsNy41l9fn3RhkWOz_B7BUXl/exec",
-		type: 		'POST',
-		data: 		finalData.serialize(),
-	});
-}
+	function setCookie() {
+		var expirationDate = new Date();
+		expirationDate.setSeconds( expirationDate.getSeconds() + 30 );
+		document.cookie = "SNAP_license_agreed=true; expires=" + expirationDate.toUTCString();
+	}
 
 
-function callback( downloadFunction ) {
-	downloadFunction();
-	var url = document.URL;
-	var filename = url.substring(url.lastIndexOf('/')+1);
-	sendFilename(filename);
+	function cookieExpired() {
+		if( document.cookie.indexOf("SNAP_license_agreed") == -1 ) {	//problem here -- atm always returning true
+			return true;
+		}
 
-}
-
-
-
-
+		else 
+			return false;
+	}
 //TODO: charts exporting window for hires png shows up before this dialog, which makes it so that it says "exporting" even after 'declined', fix?
 		//on that note -- remember that you can close that exporting dialog from anywhere with '#processingExportDialog'.dialog('close'); 
 		//possibly related -- if you click the hires download, cancel it, and then try another one, and cancel IT, the canceled window gives way to an empty download window
 				//--we'll have to see if that gets fixed when I fix the above issue.
-function licenseModal( salutation, licenseAgreement, downloadFunction ) {
-	//TODO: consider strategically placed fieldset/legend tags for formatting prettiness
-	var downloadDialog = $(document.createElement('div'));
-	downloadDialog.append( document.getElementById( salutation ) ).append( document.getElementById( "textEntry" ) )
-		.append( document.getElementById( licenseAgreement ) );
-
-	downloadDialog.show().dialog({
-		dialogClass: "no-close",
-		closeOnEscape: false,
-		modal: true,
-		draggable: false,
-		resizable: false,
-		show: 'fade',
-		hide: 'fade',
-		width: '1000px',
-		title: 'File Download',
-		zindex: 50001,	//note to self: one more than 1st level download dialogs, doesn't seem to make a difference - at least in relation to other dialogs
-							//this is actually removed in JSUI 1.10.0. in fact, all dialogs seem to be stuck at zindex=1002
-		buttons: {
-			"Accept":function() {
-				$(this).dialog('close');
-				// downloadFunction();
-				callback( downloadFunction );
-
-			},
-
-			"Decline":function() {
-				$(this).dialog('close');
-			}
-		}
-	});
-
-	
-		$(document).ready(function() { 
-
-			$('#textEntry').submit( function() {	//intercept the click of the "submit" button
 
 
-				$.ajax({
-					url: 		$(this).attr('action'),
-					type: 		$(this).attr('method'),
-					data: 		$(this).serialize(),
-				});
-				$('#textEntry').find("input[type=text], textarea").val("");	//clear form fields TODO: consider changing this to remove the form and display a thank you
-
-				return false;
-			});
+	function sendData( downloadFunction ) {
+		$.ajax({	//TODO: add validation here, and make this ajax call the submitHandler attribute of the validation call.
+			url: 		$('#textEntry').attr('action'),
+			type: 		$('#textEntry').attr('method'),
+			data: 		$('#textEntry').serialize(),
+			success: 	downloadFunction(),					//TODO: error handling? ties into above
 		});
+
+
+	}
+function licenseModal( salutation, licenseAgreement, downloadFunction ) {
+	if( cookieExpired() ) {
+
+		$('#textEntry').find("input[type=text], textarea").val("");	//clears input fields
+		var downloadDialog = $(document.createElement('div'));
+		downloadDialog.append( document.getElementById( salutation ) ).append( document.getElementById( "textEntry" ) )
+			.append( document.getElementById( licenseAgreement ) ).append( document.getElementById( "licenseNote" ) );
+
+		
+		downloadDialog.show().dialog({
+			dialogClass: "no-close",
+			closeOnEscape: false,
+			modal: true,
+			draggable: false,
+			resizable: false,
+			show: 'fade',
+			hide: 'fade',
+			width: '1000px',
+			title: 'File Download',
+			zindex: 50001,	//note to self: one more than 1st level download dialogs, doesn't seem to make a difference - at least in relation to other dialogs
+								//this is actually removed in JSUI 1.10.0. in fact, all dialogs seem to be stuck at zindex=1002
+			buttons: {
+				"Accept":function() {
+					$(this).dialog('close');
+					setCookie();
+					sendData( downloadFunction );
+
+				},
+
+				"Decline":function() {
+					$(this).dialog('close');
+				}
+			}
+		});
+	}
+	else {
+		sendData( downloadFunction );
+	}
 }
