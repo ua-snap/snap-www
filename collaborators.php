44f09dfdae71e7c3945e0e1375c979fa1251ee9a
bryce <bamelegari@alaska.edu>
2013-10-02 14:04:39 -0800

    creates some extra space between the top paragraph/image and the logo cloud, where the large ACCAP logo was deleted from. (#151)

--- collaborators.php
+++ collaborators.php
@@ -11,7 +11,7 @@ $page->pageHeader();
             <div id="main_content">
                 <div class="subHeader" style="margin-bottom: 10px;">Collaborators</div>
                 <div>
-                    <img src="images/collaborators01.jpg" style="float: left; margin-right: 15px; height: 240px; margin-bottom: 5px; padding: 3px; border: 1px solid #999999;" />
+                    <img src="images/collaborators01.jpg" style="float: left; margin-right: 15px; height: 240px; margin-bottom: 50px; padding: 3px; border: 1px solid #999999;" />
                     <p>Collaborations with the Bureau of Land Management, National Park Service, U.S. Fish and Wildlife Service, U.S. Geological Survey, Alaska Department of Fish and Game, multiple non-profit organizations, and many others have yielded scenario projections of shifting ecosystems that will aid in planning future resource, land, and wildlife management.  A variety of SNAP services and approaches make information accessible to a wide spectrum of stakeholders with varying levels of technical expertise.</p>
                     <p>SNAP continues to build a powerful collaborative network of key scientific, social and economic institutions from around the world, applying the combined expertise towards a better understanding of the future dynamics of the Arctic. <a href="people.php#contact">Contact</a> us if you are interested in joining the SNAP network.</p>
                 </div>
