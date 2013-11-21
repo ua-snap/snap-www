fc5680b808f38582ee0993de296c26a323c352af
bryce <bamelegari@alaska.edu>
2013-11-21 09:55:03 -0900

    lessens font size for button titles on analysis page as a temporary solution to #181. Also fixes some other formatting issues. (#179)

--- analysis_tools.php
+++ analysis_tools.php
@@ -9,7 +9,7 @@ $page->pageHeader();
 <div id="main_body">
 	<div id="main_content" style="min-height: 200px;" class="methods text">
 		<h2>Data Analysis Tools</h2>
-			<p>SNAP and ACCAP (<a href="http://accap.uaf.edu/" target="_blank">http://accap.uaf.edu/</a>) develop data analysis and visualization tools to aid the study of interrelated systems, improve decision making, manage resources, and inform policy. Explore and analyze historical data and SNAP climate projections with this set of dynamic presentation tools powered by R (<a href="http://www.r-project.org/" target="_blank">http://www.r-project.org/</a>) and Shiny (<a href="http://www.rstudio.com/shiny/" target="_blank">http://www.rstudio.com/shiny/</a>). Supplemental information about these and additional tools are available at the SNAP Blog (<a href="http://blog.snap.uaf.edu/" target="_blank">http://blog.snap.uaf.edu/</a>).</p>
+			<p>SNAP and <a href="http://accap.uaf.edu/" target="_blank">ACCAP</a> develop data analysis and visualization tools to aid the study of interrelated systems, improve decision making, manage resources, and inform policy. Explore and analyze historical data and SNAP climate projections with this set of dynamic presentation tools powered by <a href="http://www.r-project.org/" target="_blank">R</a> and <a href="http://www.rstudio.com/shiny/" target="_blank">Shiny</a>. Supplemental information about these and additional tools are available at the <a href="http://blog.snap.uaf.edu/" target="_blank">SNAP Blog</a>.</p>
 
 		<div class="appsWrapper">
 
@@ -18,19 +18,19 @@ $page->pageHeader();
 			<a href="http://shiny.snap.uaf.edu/sea_ice_winds/" class="appPanel">
 			<h3>Sea Ice Concentrations and Wind Events</h3>
 			<img src="images/ice-wind-icon.png"/>
-			<p>Examine projected interactions between monthly sea ice concentrations and days exhibiting extreme wind events</p>
+			<p>Examine projected interactions between monthly sea ice concentrations and days exhibiting extreme wind events.</p>
 			</a>
 
 			<a href="http://shiny.snap.uaf.edu/sea_ice_coverage/" class="appPanel" id="moo">
 			<h3>Modeled Arctic Sea Ice Coverage</h3>
 			<img src="images/sea-ice-icon.png"/>
-			<p>Compare and explore observed and model projections of arctic sea ice extent and concentration through 2099</p>
+			<p>Compare and explore observed and model projections of arctic sea ice extent and concentration through 2099.</p>
 			</a>
 
 			<a href="http://shiny.snap.uaf.edu/temp_wind_events/" class="appPanel">
 			<h3>CMIP5 Quantile-mapped Daily Data</h3>
 			<img src="images/event-freq-icon.png"/>
-			<p>Analyze the frequency of extreme daily temperature and wind events from 1958 and projected through 2100</p>
+			<p>Analyze the frequency of extreme daily temperature and wind events from 1958 and projected through 2100.</p>
 			</a>
 
 <?php
