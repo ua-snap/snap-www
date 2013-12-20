669fdc5432ab33711208fd7f5d6e6710d5004ce3
Bruce Crevensten <bruce.crevensten@gmail.com>
2013-12-20 10:49:47 -0500

    #185, add CC license to tools/data page

--- datamaps.php
+++ datamaps.php
@@ -5,41 +5,39 @@ $page->openPage();
 $page->pageHeader();
 
 ?>
-        <div id="main_body">
-            <div id="main_content" style="min-height: 200px;" class="methods text">
+<div id="main_body">
+	<div id="main_content" style="min-height: 200px;" class="methods text">
 
-            <?php echo Config::$termsOfUse; ?>
+		<h2>Tools and Data</h2>
 
-            <h2 style="margin-top: 1em;">Tools and Data</h2>
+		<p>At SNAP, we develop tools that enable you to reveal trends and patterns in climate that are of interest to a wide range of people and organizations. Please explore the tools below, or download raw data for your own analyses, and please let us know any feedback by <a href="people.php#contact">contacting us</a>.
+		</p>
 
+		<div class="toolsWrapper">
 
-<p>At SNAP, we develop tools that enable you to reveal trends and patterns in climate that are of interest to a wide range of people and organizations. Please explore the tools below, or download raw data for your own analyses, and please let us know any feedback by <a href="people.php#contact">contacting us</a>.
-</p>
+			<a href="/data.php" class="toolPanel">
+				<h3>Download Data</h3>
+				<img src="images/download-icon.png"/>
+				<p>Download projected and historical datasets in the GeoTIFF format for your own research purposes.</p>
+			</a>
 
-<div class="toolsWrapper">
+			<a href="maps.php" class="toolPanel">
+				<h3>Map Tool</h3>
+				<img src="images/map-icon-multi2.png"/>
+				<p>Visually browse and compare scenarios created from SNAP data using the interactive<br/> map tool.</p>
+			</a>
 
-<a href="/data.php" class="toolPanel">
-<h3>Download Data</h3>
-<img src="images/download-icon.png"/>
-<p>Download projected and historical datasets in the GeoTIFF format for your own research purposes.</p>
-</a>
+			<a href="/charts.php" class="toolPanel">
+				<h3>Community Charts</h3>
+				<img src="images/chart-icon.png"/>
+				<p>Investigate temperature and precipitation projections for thousands of communities across Alaska and Canada.</p>
+			</a>
 
-<a href="maps.php" class="toolPanel">
-<h3>Map Tool</h3>
-<img src="images/map-icon-multi2.png"/>
-<p>Visually browse and compare scenarios created from SNAP data using the interactive<br/> map tool.</p>
-</a>
+		</div>
+	</div>
 
-<a href="/charts.php" class="toolPanel">
-<h3>Community Charts</h3>
-<img src="images/chart-icon.png"/>
-<p>Investigate temperature and precipitation projections for thousands of communities across Alaska and Canada.</p>
-</a>
+	<?php echo Config::$termsOfUse; ?>
 
 </div>
 
-        </div>
-    </div>
-<?php
-$page->closePage();
-?>
+<?php $page->closePage(); ?>
